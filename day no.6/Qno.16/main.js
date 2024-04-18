f81fc4ea470f0d87630cfb4c3fe6187fe6388e19
Qiratsumra <saeedqirat43@gmail.com>
2024-04-18 18:52:51 +0500

    first commit

---
+++ day no.6/Qno.16/main.js
@@ -0,0 +1,27 @@
+/*More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+• Start with your program from Exercise 15.
+Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+• Add one new guest to the beginning of your array.
+
+• Add one new guest to the middle of your array.
+• Use append() to add one new guest to the end of your list.
+• Print a new set of invitation messages, one for each person in your list.*/
+//making guest list: 
+var guests = ["Saeed", "Abari", "Sumra", "Kinza"];
+// guest unable to come:
+var unableToCome = guests[1];
+// add a new guest in the replacement of guest who is not come
+guests.splice(1, 1, "Ali");
+// add a  new guest in starting of list:
+guests.unshift("Sara");
+// add a new guest in the endinng of list:
+guests.push("Sana");
+// add a new guest in the middle of the list:
+var middle = Math.floor(guests.length / 2);
+guests.splice(middle, 0, "Qirat");
+// // print the message for the guest who unable to come:
+console.log(unableToCome, "is not join us for grand dinner");
+// print message for bigger table:
+console.log("Great New");
+guests.forEach(function (guest) { return console.log("".concat(guest, " is inviting for grand dinner")); });
